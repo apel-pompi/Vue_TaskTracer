a3234880a73efafbf5f4b9d2f1846163234b7a07
apel-pompi <apel.pompi@gmail.com>
2023-12-10 18:18:49 +0600

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>Task Tracer</h1>
+        <Button text="Add Tast" color="green"></Button>
+    </header>
+</template>
+
+<script>
+    import Button from "./Button";
+    export default{
+        name:'Header',
+        components:{
+            Button
+        },
+    }
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
